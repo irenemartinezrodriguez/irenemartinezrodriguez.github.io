ae0eba77b99d7ba8569ad6c9d0f02a65973ad066
irenemartinezrodriguez <55833595+irenemartinezrodriguez@users.noreply.github.com>
2020-01-23 17:26:04 +0100

    Add files via upload

---
+++ animacion/script.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+$(window).scroll(function(){
+if ($(".animacion-4").visible(true)) {
+$(".animacion-4").addClass("visible");
+} else {
+$(".animacion-4").removeClass("visible");
+}
+});
